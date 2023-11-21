c6808c7ef6cf3e86a68930bd0a6a8d32578ce059
JangchubDorjiu <jangchubdorji17@gmail.com>
2023-11-21 10:55:48 +0600

    first

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+const availableCategories = [
+ Category(
+      id: 'c1',
+      title: 'Bhutan',
+      color: Colors.yellow,   
+ ),
+  Category(
+      id: 'c2',
+      title: 'Quick & Easy',
+      color: Colors.red,   
+ ),
+  Category(
+      id: 'c3',
+      title: 'Humburgers',
+      color: Color.fromARGB(255, 196, 46, 96),   
+ ),
+  Category(
+      id: 'c4',
+      title: 'Italian',
+      color: Colors.purple,   
+ ),
+  Category(
+      id: 'c5',
+      title: 'German',
+      color: Colors.green,   
+ ),
+  Category(
+      id: 'c6',
+      title: 'Light & Lovely',
+      color: Colors.lightBlue,   
+ ),
+  Category(
+      id: 'c7',
+      title: 'Momo',
+      color: Color.fromARGB(255, 148, 190, 135),   
+ ),
+  Category(
+      id: 'c8',
+      title: 'Noddels',
+      color: Color.fromARGB(255, 91, 76, 175),  
+ ),
+];
